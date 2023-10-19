41eebb692942379f03d5e56637c2ca7599f972e1
xxzz-tt <tanxuanzhi@gmail.com>
2023-10-19 16:07:58 -0400

    update version

--- ebdjango/polls/views.py
+++ ebdjango/polls/views.py
@@ -10,6 +10,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
+
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
