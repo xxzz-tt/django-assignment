50027adda4ca7031d9214d1e642a3f6a79038e98
xxzz-tt <tanxuanzhi@gmail.com>
2023-10-19 17:38:45 -0400

    reformat

--- ebdjango/polls/tests.py
+++ ebdjango/polls/tests.py
@@ -15,7 +15,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-    
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
